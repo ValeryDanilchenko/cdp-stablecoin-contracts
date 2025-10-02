b4e6f144076dc2b5376c380f4cde57a69840879a
valery <valerius022@gmail.com>
2025-10-02 15:58:37 +0700

    refactor: cleanup unnecessary files

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Stablecoin} from "../src/core/Stablecoin.sol";
